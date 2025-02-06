2f6c4a0bb15d3fd42a0dc9bdf8eb2842eb675187
jubayer <jubayerahmed46@gmail.com>
2025-02-06 18:51:42 +0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-6xl text-purple-600 font-bold">
+        Welcome to my Portfolio!
+      </h2>
+    </div>
+  );
+}
+
+export default App;
